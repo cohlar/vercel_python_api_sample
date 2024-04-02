6d3922748b7c6baf1b0c966dcfd09ea8d527a9db
cohlar <cohlar@gmail.com>
2024-04-02 17:12:06 +0400

    add venv

--- api/index.py
+++ api/index.py
@@ -1,8 +1,11 @@
 from http.server import BaseHTTPRequestHandler
+import pandas as pd
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        df = pd.DataFrame()
+
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
